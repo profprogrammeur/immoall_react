2526b4978e3176c12d63775f034b43f8d76f958c
RIEULoic <lrieu31@outlook.fr>
2022-06-14 21:20:31 +0400

    add hero on main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios"
-import Properties from './components/properties';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Properties from "./components/properties";
+import { useEffect, useState } from "react";
+import Hero from "./components/Hero";
 
-
-const API_URL = "http://localhost:3000/properties"
-
+const API_URL = "http://localhost:3000/properties";
 
 function getAPIData() {
-  return axios.get(API_URL).then((response) => response.data)
+  return axios.get(API_URL).then((response) => response.data);
 }
 
 function App() {
-  const [properties, setProperties] = useState([])
+  const [properties, setProperties] = useState([]);
 
   useEffect(() => {
-    let mounted = true
+    let mounted = true;
     getAPIData().then((items) => {
       if (mounted) {
         setProperties(items);
@@ -24,16 +23,13 @@ function App() {
       }
     });
     return () => (mounted = false);
-  }, [])
-
+  }, []);
 
   return (
     <div className="App">
       <h1>REACT immoALL</h1>
+      <Hero />
       <Properties properties={properties} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
     </div>
   );
 }
