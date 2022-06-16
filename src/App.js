c4e5563069bf09a6fdb5cfce3fc80fa23e2c1c93
profprogrammeur <contact@profprogrammeur.com>
2022-06-17 01:26:40 +0200

    localstorage solution cleaned

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import Properties from "./components/Properties";
 import { useEffect, useState } from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Property from "./components/Property";
-import React, { createContext, useContext } from "react";
-
-
-import UserContext from "./UserContext"
-
-// const UserContext = createContext({properties_ :[]});
 
 
 const API_URL = "http://localhost:3000/properties";
@@ -21,20 +14,10 @@ function getAPIData() {
 
 function App() {
 
-  // const [properties, setProperties] = useState(
-  //   localStorage.getItem("properties")
-  //     ? localStorage.getItem("darkMode")
-  //     : false
-  // );
-  // useEffect(() => {
-  //   localStorage.setItem("darkMode", darkMode);
-  // });
-
-  
-
   const [properties, setProperties] = useState([]);
 
   useEffect(() => {
+
     let mounted = true;
     getAPIData().then((items) => {
       if (mounted) {
@@ -44,19 +27,13 @@ function App() {
     });
     return () => (mounted = false);
   }, []);
-// let properties_="=properties
-// const UserContext2 = createContext({ properties: properties });
 
   return (
-   
-      
     <div className="App container-fluid">
       <Navbar />
       <Hero />
       <Properties properties={properties} />
     </div>
-    
-
   );
 }
 
--- src/components/pages/PropertyPage.js
+++ src/components/pages/PropertyPage.js
@@ -1,54 +1,26 @@
 import React from "react";
 import { useParams } from 'react-router';
 import Property from "../Property";
-import axios from "axios";
-import { useContext } from "react";
-import UserContext from "../../UserContext"
-
-
-
-// const API_URL = "http://localhost:3000/properties";
-// function getAPIData() {
-//   return axios.get(API_URL).then((response) => response.data);
-// }
 
 
   const PropertyPage = () => {
-    // const properties = useContext(UserContext)
-  // const [properties, setProperties] = useState([]);
-  // useEffect(() => {
-  //   let mounted = true;
-  //   getAPIData().then((items) => {
-  //     if (mounted) {
-  //       setProperties(items);
-  //       // console.log("lolitems : " + items)
-  //     }
-  //   });
-  //   return () => (mounted = false);
-  // }, []);
-
-  // console.log(properties.find(x=>x.id === 38))
-
 
   const { id } = useParams();
-  // console.log("id = " + id)
-
   const properties = JSON.parse(localStorage.getItem('properties'))
   let property = properties.find(x => x.id === parseInt(id))
-  console.log(properties)
-  console.log(properties.find(x => x.id === parseInt(id)))
+  // console.log(properties)
+  // console.log(properties.find(x => x.id === parseInt(id)))
 
   return(
     <div>
       <h1>PropertyPage</h1>
       <h2>id = {id}</h2>
-      {/* <h3>{properties[0]}</h3> */}
+  
       <Property 
         title={property.title}
         description={property.description}
         image_url={property.image_url}
         price={property.price} />
-   
     </div>
     
    );
